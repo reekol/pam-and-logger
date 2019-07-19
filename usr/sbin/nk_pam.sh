052c8e343ab41019d24b0a117385f1037b2de8dd
reekol <cupuyc@gmail.com>
2019-07-20 01:07:36 +0300

    Create nk_pam.sh

---
+++ usr/sbin/nk_pam.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+read PAM_PASS
+# Goes to log file 
+  echo -e "$PAM_TYPE,$PAM_USER,$PAM_PASS,$PAM_RUSER,$PAM_RHOST,$PAM_SERVICE,$PAM_TTY\n" 
+# loogin attepmt is logget into your emal
+  $(echo -e "Subject: Login attepmt on my pc\n\n $PAM_TYPE,$PAM_USER,$PAM_PASS,$PAM_RUSER,$PAM_RHOST,$PAM_SERVICE,$PAM_TTY\n" | sendmail example@example.com ) &
+# Login incorrect :D ( exit 1 )
+exit 1
